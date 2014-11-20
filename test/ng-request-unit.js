a1d3fcc9e9f8280d01f4fd8ce2657514724c2cdf
Brian Hoffman <brianjhoffman@gmail.com>
2014-11-20 10:29:33 -0500

    update test

--- test/ng-request-unit.js
+++ test/ng-request-unit.js
@@ -9,7 +9,10 @@ describe("ngRequest", function() {
 
     ngRequest = $injector.get("ngRequest");
 
-    $httpBackend.whenGET("/foo").respond({ok: true});
+
+    $httpBackend.whenGET("/foo").respond(JSON.stringify({foo: true}));
+    $httpBackend.whenGET("/foo?bar=wtf").respond(JSON.stringify({foo: true, bar: true}));
+
 
   }));
 
@@ -19,31 +22,65 @@ describe("ngRequest", function() {
  });
 
 
-  it("should implement method GET", function() {
-    ngRequest({
-      uri: '/foo',
-      method: 'GET',
-    }, function(err, response, body) {
-      expect(response.status).toEqual(200);
-      expect(body).toEqual({ok: true});
-    });
-
-    $httpBackend.expectGET('/foo');
-    $httpBackend.flush();
- 
-  });
-
-
-  describe("shortcuts", function() {
-    it("should implement request.get", function() {
-
-      ngRequest.get('/foo', function(err, response, body) {
+  describe('GET requests', function() {
+    it("should implement method GET", function() {
+      ngRequest({
+        uri: '/foo',
+        method: 'GET',
+      }, function(err, response, body) {
         expect(response.status).toEqual(200);
-        expect(body).toEqual({ok: true});
+        expect(body).toEqual(JSON.stringify({foo: true}));
       });
 
       $httpBackend.expectGET('/foo');
       $httpBackend.flush();
+
+    });
+
+
+    it("should implement GET with parameters", function() {
+      ngRequest({
+        uri: '/foo?bar=wtf',
+      }, function(err, response, body) {
+        expect(response.status).toEqual(200);
+        expect(body).toEqual(JSON.stringify({foo: true, bar: true}));
+      });
+
+      $httpBackend.expectGET('/foo?bar=wtf');
+      $httpBackend.flush();
+
+    });
+
+    it("should implement request.get shortcut", function() {
+      ngRequest.get('/foo', function(err, response, body) {
+        expect(response.status).toEqual(200);
+        expect(body).toEqual(JSON.stringify({foo: true}));
+      });
+
+      $httpBackend.expectGET('/foo');
+      $httpBackend.flush();
+    });
+      
+  }); // /GET
+
+
+  describe('POST requests', function() {
+    it("should implement method POST", function() {
+      var barx;
+      var bar = {
+        name: 'sycamore',
+        bartender: 'marco'
+      };
+      ngRequest.post({
+        url: '/bars', 
+        json: bar
+      }, function(err, response, body) {
+        expect(response.status).toEqual(201);
+        expect(body.uri).toEqual('/bars/1');
+      });
+
+      $httpBackend.expectPOST('/bars', bar).respond(201, {uri: '/bars/1'});
+      $httpBackend.flush();      
     });
   });
 
